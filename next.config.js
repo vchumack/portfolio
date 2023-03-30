c04d779c829f19edde6f49d9aa1cdb9af1223cff
Valeriya Druchinina <vchumack@glmedia.org>
2023-03-30 22:40:45 +0300

    fix next.config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	sassOptions: {
-		includePaths: [path.join(__dirname, "styles")],
-	},
 };
 
 module.exports = nextConfig;
